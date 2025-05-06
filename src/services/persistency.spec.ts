f0d81f3a1fdf81fbd384d27804f358cbf2b5c9a3
Marcelo Magalhaes <celoxd01@gmail.com>
2025-05-06 12:35:23 -0300

    SpyOn (Espião)

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,25 @@
-describe('Testing something', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('Testing anything', () => {
-  test('should return Luiz', () => {
-    const name = 'Luiz';
-    expect(name).toBe('Luiz');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks()); // limpar os mocks conforme finalizar cada teste
+
+  it('should return undefined', () => {
+    // System Under Test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log'); // espião
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Order saved successfully"', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Order saved successfully');
   });
 });
